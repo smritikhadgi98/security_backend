a3790331c76219965b7b543a1804e1c92ceab6f3
Smriti <smritikhadgi07@gmail.com>
2025-02-01 15:39:47 +0545

    changes

--- middleware/ActivityLog.js
+++ middleware/ActivityLog.js
@@ -1,27 +1,33 @@
 const Log = require("../models/logModel");
+
 const logRequest = async (req, res, next) => {
+  // Check if req.user exists and get the username or email
+  const username = "smritikhadgi07@gmail.com" ;
+  // const username = req.user ? req.user.userName || req.user.email || "Unknown User" : "Unknown User";
+
+  // Log entry details
   const logEntry = new Log({
-    username: req.user ? req.user.username || req.user.email || "Unknown User" : "Unknown User",
+    username: username,
     url: req.originalUrl,
     method: req.method,
-    role: req.user?.role || "User", // Dynamically set role
+    role: req.user?.role  || "User", // Dynamically set role
     status: res.statusCode,
     time: new Date(),
+
     headers: req.headers, // Include headers
     device: req.headers["user-agent"], // Include device information
     ipAddress: req.ip, // Include IP address
   });
- 
+
   try {
-    await logEntry.save();
+    await logEntry.save(); // Save the log entry to the database
   } catch (error) {
-    console.error("Error logging request:", error.message);
+    console.error("Error logging request:", error.message); // Log the error if saving fails
   }
- 
-  next();
+
+  next(); // Proceed to the next middleware or request handler
 };
 
 module.exports = {
-    
-    logRequest
-}
+  logRequest
+};
